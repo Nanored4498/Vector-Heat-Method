a52ec0738a7c61f65395509435a72c936395ed21
Nanored4498 <nanored.mc@gmail.com>
2020-03-27 13:46:43 +0100

    neighborhood

--- heat_vector.cpp
+++ heat_vector.cpp
@@ -2,6 +2,11 @@
 
 #include <igl/avg_edge_length.h>
 #include <igl/squared_edge_lengths.h>
+#include <igl/per_face_normals.h>
+
+#include <map>
+#include <vector>
+#include <iostream>
 
 template <typename DerivedV, typename DerivedF, typename Scalar>
 bool igl::heat_vector_precompute(
@@ -25,6 +30,8 @@ bool igl::heat_vector_precompute(
 	HeatVectorData<Scalar> &data) {
 		
 	typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 3> MatrixX3S;
+	typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> VectorXS;
+	typedef std::complex<Scalar> Complex;
 
 	assert(F.cols() == 3);
 	int n = V.rows(), m = F.rows(), d = V.cols();
@@ -35,14 +42,49 @@ bool igl::heat_vector_precompute(
 
 	// Face corner angles
 	MatrixX3S theta(m, 3);
+	VectorXS sumTheta = VectorXS::Zero(n);
+	std::vector<std::map<int, std::pair<int, int>>> edge_to_face(n);
 	for(int i = 0; i < m; i++) {
 		Scalar sl2 = l2.row(i).sum();
 		Scalar pl2 = l2.row(i).prod();
-		for(int j = 0; j < 3; j++)
-			theta(i, j) = 0.5 * (sl2 - l2(i, j)) / std::sqrt(pl2 / l2(i, j));
+		for(int j = 0; j < 3; j++) {
+			theta(i, j) = std::acos(.5 * (sl2 - 2*l2(i, j)) / std::sqrt(pl2 / l2(i, j)));
+			sumTheta(F(i, j)) += theta(i, j);
+			edge_to_face[F(i, j)][F(i, (j+1)%3)] = {i, j};
+		}
 	}
 
-	// std::vector<std::vector>
+	// Neighborhoods and phi
+	MatrixX3S face_normals(m, 3), vertex_normals = MatrixX3S::Zero(n, 3);
+	igl::per_face_normals(V, F, face_normals);
+	std::vector<std::vector<std::pair<int, Scalar>>> neighbors(n);
+	for(int i = 0; i < n; i++) {
+		assert(!edge_to_face[i].empty());
+		bool is_on_border = false;
+		int j0 = (*edge_to_face[i].lower_bound(0)).first;
+		for(std::pair<int, std::pair<int, int>> j : edge_to_face[i]) {
+			if(!edge_to_face[j.first].count(i)) {
+				assert(!is_on_border);
+				j0 = j.first;
+				is_on_border = true;
+			}
+			vertex_normals.row(i) += face_normals.row(j.second.first);
+		}
+		vertex_normals.row(i).normalize();
+		int j = j0;
+		Scalar phi = 0;
+		Scalar factor = 2 * M_PI / sumTheta(i);
+		do {
+			neighbors[i].emplace_back(j, phi);
+			if(!edge_to_face[i].count(j)) break;
+			std::pair<int, int> &face = edge_to_face[i][j];
+			phi += theta(face.first, face.second) * factor;
+			j = F(face.first, (face.second+2)%3);
+		} while(j != j0);
+	}
+
+	Eigen::SparseMatrix<Scalar> connexionLaplacian(n, n);
+
 
 	return heat_geodesics_precompute(V, F, t, data.scalar_data);
 
